0c2cf53faefe3a4be6df7d55453238010d1c399a
hundin-billig <158035708+hundin-billig@users.noreply.github.com>
2024-05-05 16:40:27 -0500

    audio
    Added audio for the start, accelerate, brake, stop functions

--- vehicle_app.py
+++ vehicle_app.py
@@ -11,6 +11,10 @@ from rich.console import Console
 from rich.panel import Panel
 # Initialize Console
 console = Console()
+# Install pygame
+# pip install pygame
+# Import pygame for audio control
+import pygame
 # Import vehicle module
 from vehicle import Car
 
@@ -20,6 +24,17 @@ car = Car()
 # Call the title of the program
 Car.title()
 
+# Initialize mixer for audio control
+pygame.mixer.pre_init(
+    44100,    # frequency
+    16,       # bit
+    2,        # stereo
+    4096      # buffer size
+    )
+
+#initialize pygame library
+pygame.init()
+
 # Call methods
 user_name = car.get_user_name()
 car_model = car.get_car_model()
@@ -27,6 +42,7 @@ car_seating = car.get_car_seating()
 max_speed = car.get_max_speed()
 
 # Display user name with maximum speed and number of passengers
+print()
 console.print(f"[yellow]{user_name} is driving a {car_model} that can seat {car_seating}.[yellow]")
 console.print(f"[yellow]The maximum speed of the {car_model} is {max_speed} mph.[yellow]")
 print()
@@ -35,23 +51,59 @@ print()
 while True:
 
     choice = console.input(f"[bold white]Start (E)ngine | (A)ccelerate | (B)rake | (S)top  | E(x)it: [bold white]").lower()
+    print()
     if choice == "e":
+
+        # Load soundfile
+        pygame.mixer.music.load('./assets/start.wav')
+        # Set volume to 75%
+        pygame.mixer.music.set_volume(0.75)
+        # Play sound
+        pygame.mixer.music.play()
+
         console.print(f"[bold green]The {car_model}'s engine is running.[bold green]")
         print()
+
     elif choice == "a":
+
+        # Load soundfile
+        pygame.mixer.music.load('./assets/acellerate.wav')
+        # Set volume to 75%
+        pygame.mixer.music.set_volume(0.75)
+        # Play sound
+        pygame.mixer.music.play()
         car.accelerate()
         console.print(f"[bold green]The {car_model} is going {car.get_speed()} mph.[bold green]")
         print()
+
     elif choice == "b":
+
+                # Load soundfile
+        pygame.mixer.music.load('./assets/downshift.wav')
+        # Set volume to 75%
+        pygame.mixer.music.set_volume(0.75)
+        # Play sound
+        pygame.mixer.music.play()
+
         car.brake()
         console.print(f"[bold green]The {car_model} is going {car.get_speed()} mph.[bold green]")
         print()
+
     elif choice == "s":
+
+        # Load soundfile
+        pygame.mixer.music.load('./assets/shutdown.wav')
+        # Set volume to 75%
+        pygame.mixer.music.set_volume(0.75)
+        # Play sound
+        pygame.mixer.music.play()
+
         car.stop()
         console.print(f"[bold green]The {car_model} is going {car.get_speed()} mph.[bold green]")
         print()
+
     elif choice == "x":
-        console.print(f"[bold green]Thank you for visiting our dealership. Have a good day![bold green]")
+        console.print(f"[red]Thank you for visiting our dealership. Have a good day![red]")
         print()
         break
 
